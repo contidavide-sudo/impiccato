f1889f050550792af3ccfe6075ac5dd17aa89286
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-03-02 21:13:33 +0100

    Aggiungere i file di progetto.

---
+++ impiccato/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace impiccato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sceltaDifficolta;
+            Console.WriteLine("Seleziona il livello di difficoltà:");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("| 1 - Facile (10tentativi)         |");
+            Console.WriteLine("| 2 - Medio (7 tentativi)          |");
+            Console.WriteLine("| 3 - Difficile (3 tentativi)      |");
+            Console.WriteLine("------------------------------------");
+
+            sceltaDifficolta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (sceltaDifficolta == 1)
+            {
+                Console.WriteLine("Hai scelto la modalità facile");
+            }
+            else if (sceltaDifficolta == 2)
+            {
+                Console.WriteLine("Hai scelto la modalità media");
+            }
+            else if (sceltaDifficolta == 3)
+            {
+                Console.WriteLine("Hai scelto la modalità difficile");
+            }
+        }
+    }
+}
